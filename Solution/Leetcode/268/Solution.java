0434b68eaeafb23a7ad23934a62ed6c928c9316d
Kanishk Kumar <7705kanishkkumar@gmail.com>
2025-05-18 21:11:27 +0530

    Create Solution.java

---
+++ Solution/Leetcode/268/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=n*(n+1)/2;
+        int currentSum=0;
+        for(int i=0;i<nums.length;i++){
+            currentSum +=nums[i];
+        }
+        return sum-currentSum;
+    }
+}
